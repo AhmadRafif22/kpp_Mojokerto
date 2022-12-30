68f2ee937e9f0dddbe5ce0253226ac69f83a727b
fauzanpr <fauzanpr.06@gmail.com>
2022-12-30 18:24:35 +0700

    [main page] update card UI

--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -17,8 +17,8 @@
                 <div class="card-image"><img src="{{ asset('images/NPWP.png') }}" alt="" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="text-lg font-bold">Pendaftaran <span class="text-[#FFCA19]">NPWP</span></h2>
-                    <p class="text-sm">Nomor Pokok Wajib Pajak</p>
+                    <h2 class="card-title">Pendaftaran <span class="text-[#FFCA19]">NPWP</span></h2>
+                    <p class="card-desc">Nomor Pokok Wajib Pajak</p>
                 </div>
             </div>
         </a>
@@ -27,8 +27,8 @@
                 <div class="card-image"><img src="{{ asset('images/wa-konsul.png') }}" alt="" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="text-lg font-bold">Pelayanan <span class="text-[#FFCA19]">Konsultasi</span></h2>
-                    <p class="text-sm">Konsultasi Perpajakan Via Whatsapp</p>
+                    <h2 class="card-title">Pelayanan <span class="text-[#FFCA19]">Konsultasi</span></h2>
+                    <p class="card-desc">Konsultasi Perpajakan Via Whatsapp</p>
                 </div>
             </div>
         </a>
@@ -37,8 +37,8 @@
                 <div class="card-image"><img src="{{ asset('images/kode-billing.png') }}" alt="" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="text-lg font-bold">Kode <span class="text-[#FFCA19]">Billing</span></h2>
-                    <p class="text-sm">Layanan Pembuatan Kode Billing</p>
+                    <h2 class="card-title">Kode <span class="text-[#FFCA19]">Billing</span></h2>
+                    <p class="card-desc">Layanan Pembuatan Kode Billing</p>
                 </div>
             </div>
         </a>
@@ -47,8 +47,8 @@
                 <div class="card-image"><img src="{{ asset('images/spt-tahunan.png') }}" alt="" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="text-lg font-bold">Lapor <span class="text-[#FFCA19]">SPT Tahunan</span></h2>
-                    <p class="text-sm">Pelaporan SPT Tahunan, e-Reporting</p>
+                    <h2 class="card-title">Lapor <span class="text-[#FFCA19]">SPT Tahunan</span></h2>
+                    <p class="card-desc">Pelaporan SPT Tahunan, e-Reporting</p>
                 </div>
             </div>
         </a>
@@ -57,8 +57,8 @@
                 <div class="card-image"><img src="{{ asset('images/tutorial-perpajakan.png') }}" alt="" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="text-lg font-bold">Tutorial <span class="text-[#FFCA19]">Perpajakan</span></h2>
-                    <p class="text-sm">Video Tutorial Perpajakan</p>
+                    <h2 class="card-title">Tutorial <span class="text-[#FFCA19]">Perpajakan</span></h2>
+                    <p class="card-desc">Video Tutorial Perpajakan</p>
                 </div>
             </div>
         </a>
@@ -67,8 +67,8 @@
                 <div class="card-image"><img src="{{ asset('images/form-perpajakan.png') }}" alt="" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="text-lg font-bold">Formulir <span class="text-[#FFCA19]">Perpajakan</span></h2>
-                    <p class="text-sm">Download Formulir Perpajakan</p>
+                    <h2 class="card-title">Formulir <span class="text-[#FFCA19]">Perpajakan</span></h2>
+                    <p class="card-desc">Download Formulir Perpajakan</p>
                 </div>
             </div>
         </a>
@@ -77,8 +77,8 @@
                 <div class="card-image"><img src="{{ asset('images/peraturan perpajakan.png') }}" alt="" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="text-lg font-bold">Peraturan <span class="text-[#FFCA19]">Perpajakan</span></h2>
-                    <p class="text-sm">Peraturan Perpajakan Terbaru</p>
+                    <h2 class="card-title">Peraturan <span class="text-[#FFCA19]">Perpajakan</span></h2>
+                    <p class="card-desc">Peraturan Perpajakan Terbaru</p>
                 </div>
             </div>
         </a>
@@ -87,8 +87,8 @@
                 <div class="card-image"><img src="{{ asset('images/layanan-tatap-muka.png') }}" alt="" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="text-lg font-bold">Layanan <span class="text-[#FFCA19]">Tatap Muka</span></h2>
-                    <p class="text-sm">Reservasi Tiket Layanan Tatap Muka di Kantor</p>
+                    <h2 class="card-title">Layanan <span class="text-[#FFCA19]">Tatap Muka</span></h2>
+                    <p class="card-desc">Reservasi Tiket Layanan Tatap Muka di Kantor</p>
                 </div>
             </div>
         </a>
@@ -97,8 +97,8 @@
                 <div class="card-image"><img src="{{ asset('images/faq.png') }}" alt="" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="text-lg font-bold">FAQ <span class="text-[#FFCA19]">Perpajakan</span></h2>
-                    <p class="text-sm">Tanya Jawab Perpajakan</p>
+                    <h2 class="card-title">FAQ <span class="text-[#FFCA19]">Perpajakan</span></h2>
+                    <p class="card-desc">Tanya Jawab Perpajakan</p>
                 </div>
             </div>
         </a>
