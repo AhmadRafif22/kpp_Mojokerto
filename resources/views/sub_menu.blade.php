eabe8d26926b820fa21c1ddd31d89e1bc0f32371
fauzanpr <fauzanpr.06@gmail.com>
2023-01-03 23:23:30 +0700

    [submenu page] add submenu page header and card on mobile

---
+++ resources/views/sub_menu.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Submenu</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <header class="w-screen bg-main-bg flex justify-start gap-x-5 py-2 px-4">
+        <a href="/" class="text-white font-bold"><</a>
+                <h2 class="text-white">KPP Pratama Mojokerto</h2>
+    </header>
+    <main>
+        <div class="py-5">
+            <h1 class="text-center text-lg">Pendaftaran NPWP</h1>
+        </div>
+        <div class="w-11/12 m-auto">
+            <a href="https://ereg.pajak.go.id">
+                <div class="bg-[#FFCA19] h-20 flex mb-3">
+                    <div class="bg-main-bg w-1/4 flex items-center">
+                        <img src="{{ asset('images/Vector.png') }}" alt="" class="m-auto">
+                    </div>
+                    <div class="w-3/4 px-2 flex items-center">
+                        <div class="w-full">
+                            <p class="border-b-2 border-white font-bold text-main-bg pb-1">https://ereg.pajak.go.id</p>
+                            <p class="text-xs pt-1">Aplikasi pendaftaran NPWP</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="https://ereg.pajak.go.id">
+                <div class="bg-[#FFCA19] h-20 flex mb-3">
+                    <div class="bg-main-bg w-1/4 flex items-center">
+                        <img src="{{ asset('images/Vector.png') }}" alt="" class="m-auto">
+                    </div>
+                    <div class="w-3/4 px-2 flex items-center">
+                        <div class="w-full">
+                            <p class="border-b-2 border-white font-bold text-main-bg pb-1">https://ereg.pajak.go.id</p>
+                            <p class="text-xs pt-1">Aplikasi pendaftaran NPWP</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="https://ereg.pajak.go.id">
+                <div class="bg-[#FFCA19] h-20 flex mb-3">
+                    <div class="bg-main-bg w-1/4 flex items-center">
+                        <img src="{{ asset('images/Vector.png') }}" alt="" class="m-auto">
+                    </div>
+                    <div class="w-3/4 px-2 flex items-center">
+                        <div class="w-full">
+                            <p class="border-b-2 border-white font-bold text-main-bg pb-1">https://ereg.pajak.go.id</p>
+                            <p class="text-xs pt-1">Aplikasi pendaftaran NPWP</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </main>
+</body>
+
+</html>
