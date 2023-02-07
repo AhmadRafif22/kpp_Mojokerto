743b7792584c77824ac6eb835af4f118afb34c14
DikhiAchmad <dikia656@gmail.com>
2023-02-07 19:06:52 +0700

    fix: fixing color and drag it on tailwind config

--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-
+    <meta title="title" content="kpp mojokerto">
+    <meta name="description"
+        content="kpp mojokerto merupakan sebuah sistem adminstrasi pajak yang terpadu, dengan prosedur langkah demi langkah, sistem OnlinePajak memastikan seluruh laporan akurat, dapat dibaca, dan ringkas sesuai dengan peraturan pajak Indonesia">
     <title>Main Page</title>
 </head>
 
@@ -16,7 +18,8 @@
             <img src="{{ asset('images/banner.png') }}" alt="foto anggota kpp mojokerto" class="w-screen">
             <figcaption class="bg-banner-kpp -translate-y-[100%] p-2 sm:p-6 sm:text-center lg:p-32">
                 <h1 class="text-white font-bold text-xl lg:text-3xl lg:mb-5">KPP Pratama Mojokerto</h1>
-                <p class="text-white text-[0.75rem] sm:text-sm lg:text-base">Jl. R.A Basuni No.KM, RW.5, Jampirogo, Kec. Sooko, Kab. Mojokerto
+                <p class="text-white text-[0.75rem] sm:text-sm lg:text-base">Jl. R.A Basuni No.KM, RW.5, Jampirogo, Kec.
+                    Sooko, Kab. Mojokerto
                 </p>
             </figcaption>
         </figure>
@@ -24,101 +27,104 @@
     <main class="pb-10 -mt-5 lg:pb-32 lg:-mt-40">
         <a href="#" class="card" role="link">
             <div class="card-content">
-                <div class="card-image"><img src="{{ asset('images/NPWP.png') }}" alt="" class="h-full">
+                <div class="card-image"><img src="{{ asset('images/NPWP.png') }}" alt="image-npwp" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="card-title">Pendaftaran <span class="text-[#FFCA19]">NPWP</span></h2>
+                    <h2 class="card-title">Pendaftaran <span class="text-warning">NPWP</span></h2>
                     <p class="card-desc">Nomor Pokok Wajib Pajak</p>
                 </div>
             </div>
         </a>
         <a href="{{ route('sub.konsultasi') }}" class="card" role="link">
             <div class="card-content">
-                <div class="card-image"><img src="{{ asset('images/wa-konsul.png') }}" alt="" class="h-full">
+                <div class="card-image"><img src="{{ asset('images/wa-konsul.png') }}" alt="image-wa-konsul"
+                        class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="card-title">Pelayanan <span class="text-[#FFCA19]">Konsultasi</span></h2>
+                    <h2 class="card-title">Pelayanan <span class="text-warning">Konsultasi</span></h2>
                     <p class="card-desc">Konsultasi Perpajakan Via Whatsapp</p>
                 </div>
             </div>
         </a>
         <a href="#" class="card" role="link">
             <div class="card-content">
-                <div class="card-image"><img src="{{ asset('images/kode-billing.png') }}" alt="" class="h-full">
+                <div class="card-image"><img src="{{ asset('images/kode-billing.png') }}" alt="image-kode-biling"
+                        class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="card-title">Kode <span class="text-[#FFCA19]">Billing</span></h2>
+                    <h2 class="card-title">Kode <span class="text-warning">Billing</span></h2>
                     <p class="card-desc">Layanan Pembuatan Kode Billing</p>
                 </div>
             </div>
         </a>
         <a href="#" class="card" role="link">
             <div class="card-content">
-                <div class="card-image"><img src="{{ asset('images/spt-tahunan.png') }}" alt="" class="h-full">
+                <div class="card-image"><img src="{{ asset('images/spt-tahunan.png') }}" alt="image-spt-tahunan"
+                        class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="card-title">Lapor <span class="text-[#FFCA19]">SPT Tahunan</span></h2>
+                    <h2 class="card-title">Lapor <span class="text-warning">SPT Tahunan</span></h2>
                     <p class="card-desc">Pelaporan SPT Tahunan, e-Reporting</p>
                 </div>
             </div>
         </a>
         <a href="#" class="card" role="link">
             <div class="card-content">
-                <div class="card-image"><img src="{{ asset('images/tutorial-perpajakan.png') }}" alt=""
-                        class="h-full">
+                <div class="card-image"><img src="{{ asset('images/tutorial-perpajakan.png') }}"
+                        alt="image-tutorial-perpajakan" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="card-title">Tutorial <span class="text-[#FFCA19]">Perpajakan</span></h2>
+                    <h2 class="card-title">Tutorial <span class="text-warning">Perpajakan</span></h2>
                     <p class="card-desc">Video Tutorial Perpajakan</p>
                 </div>
             </div>
         </a>
         <a href="#" class="card" role="link">
             <div class="card-content">
-                <div class="card-image"><img src="{{ asset('images/form-perpajakan.png') }}" alt=""
+                <div class="card-image"><img src="{{ asset('images/form-perpajakan.png') }}" alt="image-form-perpajakan"
                         class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="card-title">Formulir <span class="text-[#FFCA19]">Perpajakan</span></h2>
+                    <h2 class="card-title">Formulir <span class="text-warning">Perpajakan</span></h2>
                     <p class="card-desc">Download Formulir Perpajakan</p>
                 </div>
             </div>
         </a>
         <a href="#" class="card" role="link">
             <div class="card-content">
-                <div class="card-image"><img src="{{ asset('images/peraturan perpajakan.png') }}" alt=""
-                        class="h-full">
+                <div class="card-image"><img src="{{ asset('images/peraturan perpajakan.png') }}"
+                        alt="image-peraturan-perpajakan" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="card-title">Peraturan <span class="text-[#FFCA19]">Perpajakan</span></h2>
+                    <h2 class="card-title">Peraturan <span class="text-warning">Perpajakan</span></h2>
                     <p class="card-desc">Peraturan Perpajakan Terbaru</p>
                 </div>
             </div>
         </a>
         <a href="#" class="card" role="link">
             <div class="card-content">
-                <div class="card-image"><img src="{{ asset('images/layanan-tatap-muka.png') }}" alt=""
-                        class="h-full">
+                <div class="card-image"><img src="{{ asset('images/layanan-tatap-muka.png') }}"
+                        alt="image-layanan-tatap-muka" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="card-title">Layanan <span class="text-[#FFCA19]">Tatap Muka</span></h2>
+                    <h2 class="card-title">Layanan <span class="text-warning">Tatap Muka</span></h2>
                     <p class="card-desc">Reservasi Tiket Layanan Tatap Muka di Kantor</p>
                 </div>
             </div>
         </a>
         <a href="#" class="card card-lastchild" role="link">
             <div class="card-content">
-                <div class="card-image"><img src="{{ asset('images/faq.png') }}" alt="" class="h-full">
+                <div class="card-image"><img src="{{ asset('images/faq.png') }}" alt="image-faq" class="h-full">
                 </div>
                 <div class="card-text">
-                    <h2 class="card-title">FAQ <span class="text-[#FFCA19]">Perpajakan</span></h2>
+                    <h2 class="card-title">FAQ <span class="text-warning">Perpajakan</span></h2>
                     <p class="card-desc">Tanya Jawab Perpajakan</p>
                 </div>
             </div>
         </a>
     </main>
     <footer>
-        <p class="bg-main-bg text-white text-sm text-center py-1">&copy; KPP Pratama Mojokerto | 2022</p>
+        <p class="bg-main-bg text-white text-sm text-center py-4">&copy; KPP Pratama Mojokerto | 2022</p>
     </footer>
 </body>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-  ],
-  theme: {
-    extend: {
-        colors: {
-            'banner-kpp': 'rgba(32, 44, 95, 0.75)',
-            'main-bg': '#202C5F',
-            'main-bg-hover': '#263471',
-        }
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'banner-kpp': 'rgba(32, 44, 95, 0.75)',
+                'main-bg': '#202C5F',
+                'main-bg-hover': '#263471',
+                'warning': '#FFCA19',
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
